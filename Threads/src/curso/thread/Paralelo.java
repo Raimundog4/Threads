d400eeb0116b01960d0381f0a321e471cf7ad1fd
raimu <raimu@LAPTOP-2NKS05GS>
2021-06-14 16:18:07 -0300

    Processamento concorrente das Threads

--- Threads/src/curso/thread/Paralelo.java
+++ Threads/src/curso/thread/Paralelo.java
@@ -6,28 +6,52 @@ public class Paralelo {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		/* Thread processando em paralelo */
+		/* Thread processando em paralelo do envio do e-mail */
 		new Thread() {
 
-			public void run() {// Executa o que nós queremos
-				/* Dentro do run vai o código da rotina */
+			public void run() {
+				
 
 				for (int pos = 0; pos < 10; pos++) {
 
 					try {
-						Thread.sleep(1000);// Ele pede para ser colocado no bloco Try/Catch
+						Thread.sleep(1000);// 
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
 					System.out.println("Executando alguma rotina, por exemplo o envio de um e-mail");
 
-					// Thread.sleep(1000);//Executa e espera
+					
 				}
 
 			};
 
-		}.start();// Start liga a thread que fica processando paralelamente por trás
+		}.start();
 
+		//***************DIVISÃO DAS THREADS
+		/* Thread processando em paralelo do envio de nota fiscal */
+		new Thread() {
+
+			public void run() {
+				
+
+				for (int pos = 0; pos < 10; pos++) {
+
+					try {
+						Thread.sleep(1000);// 
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println("Executando alguma rotina, por exemplo o envio de nota fiscal");
+
+					
+				}
+
+			};
+
+		}.start();
+
+		
 		System.out.println("Chegou ao fim do código de teste de Thread");
 
 		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
